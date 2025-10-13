5d1df7227b70bef1a692cfc4f3c3113cc90f492f
ben oliver <codepratice851@gmail.com!>
2025-10-13 16:55:45 +0100

    twilio creentials updated

---
+++ twilio_app.py
@@ -0,0 +1,18 @@
+from twilio.rest import Client
+import os
+
+account_sid = os.getenv("TWILIO_ACCOUNT_SID")
+auth_token = os.getenv("TWILIO_AUTH_TOKEN")
+print("SID:", account_sid)
+print("Token length:", len(auth_token) if auth_token else "None")
+
+client = Client(account_sid, auth_token)
+
+message = client.messages.create(
+  from_='whatsapp:+14155238886',
+  content_sid='HXb5b62575e6e4ff6129ad7c8efe1f983e',
+  content_variables='{"1":"12/1","2":"3pm"}',
+  to='whatsapp:+447565708252'
+)
+
+print(message.sid)
